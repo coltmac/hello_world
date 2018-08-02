8f5449c0edfff68c81a8d6774d28c7bac751e481
coltmac <macm.colin@gmail.com>
2018-08-02 17:01:54 -0600

    This was a test of the cmd line command: "git commit ." while in the GitHub\hello_world repository

--- run_helloworld.sql
+++ run_helloworld.sql
@@ -1,2 +1,4 @@
 --select 'Hello World', 1 id
-select 'Hello World', 2 id --v2
+--select 'Hello World', 2 id --v2
+/*test 20180802 version control from centurylink office */
+select 'Hello World', 3 id --v3
